4d2597532ffe1369bb2ea6007878a460cefe55ae
An Nguyen <57944776+an310898@users.noreply.github.com>
2023-05-12 19:17:45 +0700

    update add new table form

--- admin/js/product-table.js
+++ admin/js/product-table.js
@@ -50,9 +50,52 @@ function hideProduct(bookId) {
     .then(res => res.json())
     .then(x => {
       if (x["#update-count-1"] > 0) {
-        // alert("Thành công");
-        showSwal("Ẩn thành công", "", "success");
+        alert("Ẩn thành công");
+
         getBookTable();
       }
     });
+}
+function hideAddForm() {
+  document.querySelector("#formAddNew").style.display = "none";
+}
+function displayAddNewForm() {
+  document.querySelector("#formAddNew").style.display = "block";
+}
+function addNewProduct() {
+  const name = document.querySelector("#BookName").value;
+  const Description = document.querySelector("#Description").value;
+  const Image = document.querySelector("#Image").value;
+  const Author = document.querySelector("#Author").value;
+  const Price = document.querySelector("#Price").value;
+  const Age = document.querySelector("#Age").value;
+  const Language = document.querySelector("#Language").value;
+  const Pages = document.querySelector("#Pages").value;
+  const PublishingCompany = document.querySelector("#PublishingCompany").value;
+  const CoverType = document.querySelector("#CoverType").value;
+  const BookWeight = document.querySelector("#BookWeight").value;
+  const BookSize = document.querySelector("#BookSize").value;
+  const CategoryIdArrays = document.querySelector("#CategoryIdArrays").value;
+  const formData = {
+    Name: name,
+    Description: Description,
+    Image: Image,
+    Author: Author,
+    Price: Price,
+    Age: Age,
+    Language: Language,
+    Pages: Pages,
+    PublishingCompany: PublishingCompany,
+    CoverType: CoverType,
+    BookWeight: BookWeight,
+    BookSize: BookSize,
+    CategoryIdArrays: CategoryIdArrays,
+  };
+  console.log(formData);
+  return;
+  fetch("http://1.52.115.73:8080/api/dynamic-procedure/AddNewBook", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+  });
 }
